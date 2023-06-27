6d94996c498464716dcc063319eff5df16b220eb
Edilson Almeida <edifilho1409@outook.com>
2023-06-27 16:20:47 -0300

    new: Work Seccion

---
+++ src/data/data.ts
@@ -0,0 +1,43 @@
+import Delivery from "../assets/projects/delivery.png";
+import EletronicStore from "../assets/projects/eletronicstore.png";
+import GitHubRequests from "../assets/projects/githubrequests.png";
+import MyBlog from "../assets/projects/myblog.png";
+import Portfolio from "../assets/projects/portfolio.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "Delivery Project",
+    image: Delivery,
+    github: "https://github.com/diinhoalmeida/delivery-project",
+    live: "https://delivery-project-two.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Eletronic Store",
+    image: EletronicStore,
+    github: "https://github.com/diinhoalmeida/shopping-cart",
+    live: "https://get-git-users.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "GitHub Requests",
+    image: GitHubRequests,
+    github: "https://github.com/diinhoalmeida/get-git-users",
+    live: "https://shopping-cart-drab-rho.vercel.app/store",
+  },
+  {
+    id: 4,
+    name: "MyBlog",
+    image: MyBlog,
+    github: "https://github.com/diinhoalmeida/blog-edilson-almeida",
+    live: "https://blog-edilson-almeida.vercel.app/",
+  },
+  {
+    id: 5,
+    name: "Portfolio",
+    image: Portfolio,
+    github: "https://github.com/diinhoalmeida/portfolio-project",
+    live: "",
+  },
+];
